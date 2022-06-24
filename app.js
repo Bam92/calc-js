70f0e7bcba4400c279a72899fa4fca674085b91c
Abel Lifaefi Mbula <abelmbula@protonmail.com>
2022-06-24 22:20:49 +0200

    feat: create functions for each arithmetic operation

---
+++ app.js
@@ -0,0 +1,21 @@
+// addition 
+function add(a, b) {
+    return a + b
+}
+
+// substraction
+function substract(a, b) {
+    return a - b
+}
+
+// multiply
+function multiply(a, b) {
+    return a * b
+}
+
+// division
+function divise(a, b) {
+    return a / b
+}
+
+console.log(add(24, 4));
